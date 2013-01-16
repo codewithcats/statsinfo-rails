9dc771a45edf08fe7691646699f0a8d12cd4df90
Tanawat Tassana <amp.tanawat@gmail.com>
2013-01-16 19:37:26 +0700

    add relationship between sale and customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
 	belongs_to :customer_area
 	belongs_to :customer_group
+	has_many :sales, dependent: :destroy
   attr_accessible :address, :area_id, :buy, :buy_avg, :cont, :contact, :credit_limit, :customer_code, :email, :fax, :group_id, :late, :name, :order, :order_avg, :phone, :quanbuy, :quanbuy_avg, :start_date
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,4 @@
 class Sale < ActiveRecord::Base
+	belongs_to :customer
   attr_accessible :amount, :customer_id, :date, :remark, :sale_code, :time_code, :user_code, :vat_amount, :vat_rate
 end
