9649049b8378d0a2387069eea3e2891d3916f9cf
Tanawat Tassana <amp.tanawat@gmail.com>
2013-01-16 20:10:56 +0700

    add relationship from sale_line to sale and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :product_group
 	belongs_to :product_category
+	has_many :sale_lines, dependent: :destroy
   attr_accessible :code, :description, :name_eng, :name_th, :price, :product_cat_id, :product_group_id
 end
