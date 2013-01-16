0adcc0f9a2e2af1a0721160cf8cc71bc1015e39e
Tanawat Tassana <amp.tanawat@gmail.com>
2013-01-16 19:16:41 +0700

    link Customer with group and area

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
+	belongs_to :customer_area
+	belongs_to :customer_group
   attr_accessible :address, :area_id, :buy, :buy_avg, :cont, :contact, :credit_limit, :customer_code, :email, :fax, :group_id, :late, :name, :order, :order_avg, :phone, :quanbuy, :quanbuy_avg, :start_date
 end
--- app/models/customer_area.rb
+++ app/models/customer_area.rb
@@ -1,3 +1,4 @@
 class CustomerArea < ActiveRecord::Base
+	has_many :customers, dependent: :destroy
   attr_accessible :area_code, :name
 end
