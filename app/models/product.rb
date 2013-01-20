c207a6904230b7290c4f731417beae69fbc2c369
Tanawat Tassana <amp.tanawat@gmail.com>
2013-01-21 02:43:59 +0700

    display name of product group and category instead of id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :product_group
-	belongs_to :product_category
+	belongs_to :product_category, foreign_key: :product_cat_id
 	has_many :sale_lines, dependent: :destroy
   attr_accessible :code, :description, :name_eng, :name_th, :price, :product_cat_id, :product_group_id
 end
