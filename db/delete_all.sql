2f0024c48e439bfee8301cfac2f9596e4f03d2b6
Tanawat Tassana <amp.tanawat@gmail.com>
2013-01-21 19:45:28 +0700

    include delete all sql script

---
+++ db/delete_all.sql
@@ -0,0 +1,10 @@
+delete from customers;
+delete from customer_areas;
+delete from customer_groups;
+
+delete from products;
+delete from product_categories;
+delete from product_groups;
+
+delete from sale_lines;
+delete from sales;
